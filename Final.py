016ea7eacc8e9787a23a0ab52d5e8b47464fd4a7
skarnik5742 <skarnik5742@gmail.com>
2026-05-27 15:53:28 -0700

    Changed Convolution Vals

--- Final.py
+++ Final.py
@@ -131,12 +131,12 @@ plt.show()
 class ConvNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 3, 1, 1)
-        self.conv2 = nn.Conv2d(6, 16, 3, 1, 1)
-        self.conv3 = nn.Conv2d(16, 48, 3, 1, 1)
+        self.conv1 = nn.Conv2d(28, 56, 3, 1, 1)
+        self.conv2 = nn.Conv2d(56, 112, 3, 1, 1)
+        self.conv3 = nn.Conv2d(112, 224, 3, 1, 1)
         self.pool = nn.MaxPool2d(2,2)
-        self.fc1 = nn.Linear(28 * 28 * 48, 400)
-        self.fc2 = nn.Linear(400, 28)
+        self.fc1 = nn.Linear(28 * 28 * 224, 1000)
+        self.fc2 = nn.Linear(1000, 28)
         self.relu = nn.ReLU()
     
     def forward(self, x):
